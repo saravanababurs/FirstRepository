c0671551f0a6e5dceb484da7a1828009d4e77dde
BABU <BABU@Saravana>
2018-04-14 21:00:37 +0530

    First Commit

---
+++ firstProgram/src/test/java/com/framework/firstProgram/AppTest.java
@@ -0,0 +1,16 @@
+package com.framework.firstProgram;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   
+{
+    @Test
+    public void firstMethod()
+    {
+    	System.out.println("First Git");
+    }
+}
